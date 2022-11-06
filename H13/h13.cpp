3e71ff010f6f3139d5dc9929e9c1a3e44c6c1254
MaksMit <mitiakin@inbox.lv>
2022-11-06 16:56:26 +0200

    H13 test

--- H13/h13.cpp
+++ H13/h13.cpp
@@ -45,3 +45,40 @@ int main()
         out << i.first << " " << i.second << endl;
     out.close();
 }
+
+/* TEST Data
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+|    Input1      /    Input2         |            Output                        |  Passed / Failed  |
++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+1 Merge1              20 Merge2                  1 Merge1 files2                          +
+2 both1               19 both2                   2 both1 both2
+3 given1              18 given2                  3 given1 from2
+4 files1              17 files2                  4 files1 saved2
+5 into1               16 into2                   5 into1 are2
+6 another1            15 another2                6 another1 ID2
+7 text1               14 text2                   7 text1 same2
+8 file1               13 file2                   8 file1 the2
+9 where1              12 where2                  9 where1 with2
+10 all1               11 all2                    10 all1 words2
+11 words1             10 words2                  11 words1 all2
+12 with1              9 with2                    12 with1 where2
+13 the1               8 the2                     13 the1 file2
+14 same1              7 same2                    14 same1 text2
+15 ID1                6 ID2                      15 ID1 another2
+16 are1               5 are2                     16 are1 into2
+17 saved1             4 saved2                   17 saved1 files2
+18 from1              3 from2                    18 from1 given2
+19 both1              2 both2                    19 both1 both2
+20 files1             1 files2                   20 files1 Merge2
+-----------------------------------------------------------------------
+
+1 Merge1              20 Merge2                  1 Merge1 files2                          +
+2 both1               19 both2                   2 both1 both2
+3 given1              18 given2                  3 given1 from2
+18 from1              3 from2                    18 from1 given2
+19 both1              2 both2                    19 both1 both2
+20 files1             1 files2                   20 files1 Merge2
+-----------------------------------------------------------------------
+*/
